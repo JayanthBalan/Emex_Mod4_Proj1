829fd9ad5827237940577e4397e10acc83b21a2c
JayanthBalan <jayanthsb.005@gmail.com>
2026-09-17 13:21:37 +0000

    bugs fixed 4: documentation mistake and subtraction variable naming inconsistency

--- src/subtraction.c
+++ src/subtraction.c
@@ -43,13 +43,13 @@ ret_types_e subtraction(Dlist_t *x, Dlist_t *y, Dlist_t *z) {
 
 ret_types_e subtractBase(Dlist_t *x, Dlist_t *y, Dlist_t *z) {
     node_t *ux = x->tail, *uy = y->tail;
-    int8_t carry = 0, diff = 0;
+    int8_t borrow = 0, diff = 0;
 
-    while(ux != NULL || uy != NULL || carry != 0) {
+    while(ux != NULL || uy != NULL || borrow != 0) {
         diff = (int8_t)((ux != NULL) ? ux->data : 0);
-        diff -= (int8_t)carry;
+        diff -= (int8_t)borrow;
         diff -= (int8_t)((uy != NULL) ? uy->data : 0);
-        carry = (diff < 0) ? 1 : 0;
+        borrow = (diff < 0) ? 1 : 0;
         diff = (diff < 0) ? (diff + 10) : diff;
         
         if(insert_beg(z, (uint8_t)diff) == fail) {
